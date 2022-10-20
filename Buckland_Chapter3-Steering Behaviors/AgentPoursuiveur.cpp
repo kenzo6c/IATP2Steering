7a6672430a7fbc9ccd40841eb1c35b2e27c416ee
Emile Veillette <emileveillette@icloud.com>
2022-10-20 15:05:03 -0700

    Added project

---
+++ Buckland_Chapter3-Steering Behaviors/AgentPoursuiveur.cpp
@@ -0,0 +1,24 @@
+#include "Vehicle.h"
+
+using namespace std;
+
+class AgentPoursuiveur
+{
+public:
+	AgentPoursuiveur()
+	{
+
+	}
+	~AgentPoursuiveur();
+
+private:
+
+};
+
+AgentPoursuiveur::AgentPoursuiveur()
+{
+}
+
+AgentPoursuiveur::~AgentPoursuiveur()
+{
+}
