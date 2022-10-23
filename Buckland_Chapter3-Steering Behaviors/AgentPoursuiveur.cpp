4fb1e70bf65cb961c2b02cba6d250aaa5031fe2a
Emile Veillette <emileveillette@icloud.com>
2022-10-22 18:10:32 -0700

    Added AgentPoursuiveur

--- Buckland_Chapter3-Steering Behaviors/AgentPoursuiveur.cpp
+++ Buckland_Chapter3-Steering Behaviors/AgentPoursuiveur.cpp
@@ -1,24 +1,18 @@
-#include "Vehicle.h"
+#include "AgentPoursuiveur.h"
 
 using namespace std;
 
-class AgentPoursuiveur
+AgentPoursuiveur::AgentPoursuiveur(GameWorld* world,
+    Vector2D position,
+    double    rotation,
+    Vector2D velocity,
+    double    mass,
+    double    max_force,
+    double    max_speed,
+    double    max_turn_rate,
+    double    scale,
+    Vehicle* nextVehicle) : Vehicle(world, position, rotation, velocity, mass, max_force, max_speed, max_turn_rate, scale) 
 {
-public:
-	AgentPoursuiveur()
-	{
-
-	}
-	~AgentPoursuiveur();
-
-private:
-
-};
-
-AgentPoursuiveur::AgentPoursuiveur()
-{
+    Vehicle::Steering()->OffsetPursuitOn(nextVehicle, Vector2D(3,2));
 }
 
-AgentPoursuiveur::~AgentPoursuiveur()
-{
-}
---
+++ Buckland_Chapter3-Steering Behaviors/AgentPoursuiveur.h
@@ -0,0 +1,23 @@
+#include "Vehicle.h"
+#include "ParamLoader.h"
+#include "SteeringBehaviors.h"
+
+using namespace std;
+
+class AgentPoursuiveur : public Vehicle
+{
+public:
+    AgentPoursuiveur(GameWorld* world,
+        Vector2D position,
+        double    rotation,
+        Vector2D velocity,
+        double    mass,
+        double    max_force,
+        double    max_speed,
+        double    max_turn_rate,
+        double    scale, Vehicle* nextVehicle);
+
+private:
+    vector<Vehicle*> m_vehicles;
+    GameWorld* gameWorld;
+};
